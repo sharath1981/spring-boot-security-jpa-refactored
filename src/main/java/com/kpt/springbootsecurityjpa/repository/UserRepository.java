60f5f49afd66b8a5455a4373db76fc9d49ddafaa
SHARATH KUMAR B <sharath1981@gmail.com>
2021-02-18 20:52:05 +0530

    Refactored

--- src/main/java/com/kpt/springbootsecurityjpa/repository/UserRepository.java
+++ src/main/java/com/kpt/springbootsecurityjpa/repository/UserRepository.java
@@ -6,7 +6,9 @@ import com.kpt.springbootsecurityjpa.model.User;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     @EntityGraph(attributePaths = {"authorities"}) 
     Optional<User> findByUsername(String username);
